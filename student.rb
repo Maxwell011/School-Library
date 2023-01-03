910d90da06f8fb037d62aa5b5171086c6c905e39
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-01-03 15:17:12 +0100

    Added changes reviewer requested

--- student.rb
+++ student.rb
@@ -1,9 +1,9 @@
 require './person'
 
 class Student < Person
-  def initialize(age,id,classroom, name = 'Unknown', parents_permission: true)
+  def initialize(age, id, classroom, name = 'Unknown', parents_permission: true)
     @classroom = classroom
-    super(age,id, name, parents_permission: parents_permission)
+    super(age, id, name, parents_permission: parents_permission)
   end
 
   def play_hooky
