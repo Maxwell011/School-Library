8d20b37a9a056ccf828dc97d65263bb47b9ec928
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-01-03 09:57:59 +0100

    Created person class

---
+++ person.rb
@@ -0,0 +1,23 @@
+class person 
+   attr_accessor :id, :name, :age
+
+  def initialize(id, name, age, parents_permission = 'Unknown')
+    @id = id,
+          @name = name,
+          @age = age,
+          @parents_permission = parents_permission
+  end
+
+  private
+
+  def of_age?
+    return true if @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parents_permission == true
+  end
+
+end
