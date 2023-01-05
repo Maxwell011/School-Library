9030ca4cc8cdceb51e608d051b9ef82fdc40f60b
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-01-05 09:44:42 +0100

    Modified the student file

--- student.rb
+++ student.rb
@@ -1,9 +1,16 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(id, name, age, classroom, parents_permission = 'Unknown')
     @classroom = classroom
     super(id, name, age, parents_permission)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
